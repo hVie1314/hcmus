06c8b946dd2b986601e54ffad64d7498b4dcdedd
hVie1314 <88617436+hVie1314@users.noreply.github.com>
2023-11-21 15:56:49 +0700

    Update TH DSA.cpp

--- ctdlgt_source_code/TH DSA.cpp
+++ ctdlgt_source_code/TH DSA.cpp
@@ -1,1 +1,56 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
 
+struct Node {
+    int data;
+    Node* pLeft;
+    Node* pRight;
+};
+
+int height(Node* root) {
+    if (!root) return 0;
+    return 1 + std::max(height(root->pLeft), height(root->pRight));
+}
+
+void add(Node*& tree, int data) {
+    
+    if (!tree){
+        Node* p=new Node;
+        p->data=data;
+        p->pLeft=NULL;
+        p->pRight=NULL;
+        tree=p;
+    }
+    else{
+        
+        if(data<tree->data) add(tree->pLeft,data);
+        if(data>tree->data) add(tree->pRight,data);
+    }
+}
+
+bool isAVL(Node* tree) {
+    if (!tree) return 1;
+    if (abs(height(tree->pLeft) - height(tree->pRight)) >=2) {
+        return 0;
+    }
+    isAVL(tree->pLeft);
+    isAVL(tree->pRight);
+}
+
+int main() {
+    Node* tree = NULL;
+    add(tree, 7);
+    add(tree, 5);
+    add(tree, 9);
+    add(tree, 6);
+    add(tree, 4);
+    add(tree, 3);
+    add(tree, 10);
+    add(tree, 11);
+    //dd(tree, 12);
+    add(tree, 8);
+    std::cout << height(tree) << std::endl;
+    std::cout << height(tree->pLeft) << " " << height(tree->pRight);
+    std::cout << isAVL(tree);
+    return 0;
+}
